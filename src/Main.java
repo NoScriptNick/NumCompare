bcb9f9be99cced793faf59a2939291a59b75033f
613668 <beres.613668@swl.k12.oh.us>
2023-09-28 09:36:37 -0400

    xcdf

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.lang.Math;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        //variables
+        int numOne;
+        int numTwo;
+
+        //asking for numbers
+        System.out.println("Enter a number: ");
+        if (scan.hasNextInt()) {
+            numOne = scan.nextInt();
+            System.out.println("Enter another number: ");
+            if (scan.hasNextInt()) {
+                numTwo = scan.nextInt();
+                if (numOne > numTwo) {
+                    System.out.println(numOne + " is greater than " + numTwo);
+                } else if (numTwo > numOne) {
+                    System.out.println(numTwo + " is greater than " + numOne);
+                } else
+                    System.out.println(numOne + " is equal to " + numTwo);
+            } else
+                System.out.println("THAT'S NOT A NUMBER");
+        } else
+            System.out.println("THAT'S NOT A NUMBER");
     }
 }
